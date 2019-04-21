0f6aadab12032843ef90747ac84a5d21dca70f11
MiniDigger <MiniDigger@users.noreply.github.com>
2019-04-21 18:47:14 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.6ca1190c87aebd7117a950c070c0a919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VueMoney/robots.txt"
+  },
+  {
+    "revision": "4f5167bda592d3d19c2f",
+    "url": "/VueMoney/js/chunk-vendors.c1a65956.js"
+  },
+  {
+    "revision": "c626e5acb93754e20f33",
+    "url": "/VueMoney/js/app.ad2d09e0.js"
+  },
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/VueMoney/js/about.ad763791.js"
+  },
+  {
+    "revision": "609ad7ddccfc5a291d3adc6327fd5666",
+    "url": "/VueMoney/index.html"
+  },
+  {
+    "revision": "4f5167bda592d3d19c2f",
+    "url": "/VueMoney/css/chunk-vendors.0d12c19e.css"
+  }
+];
