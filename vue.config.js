635dc38bc8d088bc10f062386826fabef3c6039b
MiniDigger <admin@minidigger.me>
2019-05-11 11:25:10 +0200

    add backend, change project layout

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  outputDir: 'dist',
-  publicPath: '/VueMoney/',
-  devServer: {
-    port: 8080,
-    disableHostCheck: true,
-  },
-};
---
+++ vuemoney-frontend/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  outputDir: 'dist',
+  publicPath: '/VueMoney/',
+  devServer: {
+    port: 8080,
+    disableHostCheck: true,
+  },
+};
